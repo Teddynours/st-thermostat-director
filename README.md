df3cb807fd66cdf3b3bbb6a74541091290f53baf
eric <dev@ebessette.com>
2018-05-05 14:49:23 -0700

    Fixed copy/paste syntax errors

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
+Originally from https://community.smartthings.com/t/control-thermostat-fan-based-on-temperature-difference/49460/5
+
 # Install Instructions
 
-Log into Smarthings IDE
-Setup Github integration
-Add repo
-Update from repo
-?
+1. Log into Smarthings IDE, https://graph.api.smartthings.com/
+1. Click on "My SmartApps" tab
+1. Setup Github integration TODO
+1. Add this repo to settings TODO
+1. Click on "Update from Repo" button
+1. Check "smartapps/temperature-difference-fan-control.src/temperature-difference-fan-control.groovy"
+1. Check "Publish"
+1. Click on "Execute Update" button
--- smartapps/temperature-difference-fan-control.src/temperature-difference-fan-control.groovy
+++ smartapps/temperature-difference-fan-control.src/temperature-difference-fan-control.groovy
@@ -1,31 +1,32 @@
 /**
  Duane’s Thermostat Fan Director
- Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except
- in compliance with the License. You may obtain a copy of the License at:
- http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ in compliance with the License. You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+ 
  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  for the specific language governing permissions and limitations under the License.
  */
 
 // Automatically generated. Make future change here.
 
 definition(
-		name: “Duanes Thermostat Fan Director”,
-namespace: “smartthings”,
-author: “Tim Slagle Modified by: Duane Helmuth”,
-description: “Adjust your thermostat fan based on the temperature of a secondary temperature sensor”,
-category: “Green Living”,
-iconUrl: “http: //icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png”,
-iconX2Url: “http: //icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png”
+	name: "temperature-difference-fan-control",
+	namespace: "er0ckx",
+	author: "Tim Slagle Modified by: Duane Helmuth",
+	description: "Adjust your thermostat fan based on the temperature of a secondary temperature sensor",
+	category: "Green Living",
+	iconUrl: "http://icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png",
+	iconX2Url: "http://icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png"
 )
 
 section {
-	input“ thermostat”, “capability.thermostat”, title: “Which Thermostat ? ”, multi : false, required: true
-	input“ tempSensor”, “capability.temperatureMeasurement”, title: “Which temperature sensor ? ”, multi : false, required: true
-	input“ tempDiff”, “number”, title: “Temperature Difference to turn fan on ? ”, required : true, defaultValue: 2
-	input“ sendPushMessage”, “bool”, title: “Send a push notification ? ”, required : false, defaultValue: true
-	input“ sendSMS”, “phone”, title: “Send as SMS ? ”, required : false, defaultValue: false
+	input" thermostat", "capability.thermostat", title: "Which Thermostat ? ", multi : false, required: true
+	input" tempSensor", "capability.temperatureMeasurement", title: "Which temperature sensor ? ", multi : false, required: true
+	input" tempDiff", "number", title: "Temperature Difference to turn fan on ? ", required : true, defaultValue: 2
+	input" sendPushMessage", "bool", title: "Send a push notification ? ", required : false, defaultValue: true
+	input" sendSMS", "phone", title: "Send as SMS ? ", required : false, defaultValue: false
 
 }
 
@@ -40,10 +41,10 @@ def updated() {
 	init()
 }
 
-def init() {
-	runIn(60, “temperatureHandler”)
-	subscribe(tempSensor, “temperature”, temperatureHandler)
-	subscribe(thermostat, “temperature”, temperatureHandler)
+def initialize() {
+	runIn(60, "temperatureHandler")
+	subscribe(tempSensor, "temperature", temperatureHandler)
+	subscribe(thermostat, "temperature", temperatureHandler)
 }
 
 def temperatureHandler(evt) {
@@ -73,8 +74,8 @@ def temperatureHandler(evt) {
 		if (currentThermFan != "fanOn") {
 			//set the therm fan state to ON
 			def msg = "I changed your ${thermostat} fan mode to ON because temperature is out of range from ${tempSensor}"
-			thermostat ? .fanOn()
-			thermostat ? .poll()
+			thermostat?.fanOn()
+			thermostat?.poll()
 			log.debug msg
 			sendMessage(msg)
 		}
@@ -82,8 +83,8 @@ def temperatureHandler(evt) {
 		if (currentThermFan != "fanAuto") {
 			//set the therm fan state to AUTO
 			def msg = "I changed your ${thermostat} fan mode to AUTO because temperature difference is in range of ${tempSensor}"
-			thermostat ? .fanAuto()
-			thermostat ? .poll()
+			thermostat?.fanAuto()
+			thermostat?.poll()
 			log.debug msg
 			sendMessage(msg)
 		}
